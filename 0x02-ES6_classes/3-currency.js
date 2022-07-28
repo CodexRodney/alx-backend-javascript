9ad8aa0322932c943d257fdc8b479b986475bece
CodexRodney <rodney002mwanje@gmail.com>
2022-07-28 18:40:25 +0300

    Implemented coding style

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,30 +1,30 @@
 export default class Currency {
-	constructor(code, name) {
-		this._code = code;
-		this._name = name;
-	}
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-	// get method for code
-	get code() {
-		return this._code;
-	}
+  // get method for code
+  get code() {
+    return this._code;
+  }
 
-	// set method for code
-	set code(code) {
-		this._code = code;
-	}
+  // set method for code
+  set code(code) {
+    this._code = code;
+  }
 
-	// get method for name
-	get name() {
-		return this._name;
-	}
+  // get method for name
+  get name() {
+    return this._name;
+  }
 
-	// set method for name
-	set name(name) {
-		this._name = name;
-	}
+  // set method for name
+  set name(name) {
+    this._name = name;
+  }
 
-	displayFullCurrency() {
-		return `${this._name} ${this._code}`;
-	}
+  displayFullCurrency() {
+    return `${this._name} ${this._code}`;
+  }
 }
