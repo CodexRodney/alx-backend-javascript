c3dc2d47e0c543250c4ed1865dbfa643a1046a30
CodexRodney <rodney002mwanje@gmail.com>
2022-08-03 01:08:53 +0300

    Added 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,9 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-	let count = weakMap.get(endpoint) === undefined ? 0 : weakMap.get(endpoint);
-	weakMap.set(endpoint, count + 1);
-	if( count >= 5 ) {
-		throw new Error("Endpoint load is high");
-	}
+  const count = weakMap.get(endpoint) === undefined ? 0 : weakMap.get(endpoint);
+  weakMap.set(endpoint, count + 1);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
