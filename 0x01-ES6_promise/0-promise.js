02dd061146d98f2a0c4ca713f0642ec0d29dd187
CodexRodney <rodney002mwanje@gmail.com>
2022-11-23 11:22:37 +0300

    My First JS Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+/*
+ * Defines a function getResponseFromAPI
+ * which returns a Promise
+ */
+
+export function getResponsefromAPI() {
+	return new Promise((resolve, reject) => {
+		resolve();
+		reject();
+	});
+}
